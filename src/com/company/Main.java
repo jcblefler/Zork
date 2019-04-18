f0bd82cae429a75898926fc3c8973a1dd1374fb0
Jacob Lefler <jcb.lefler@gmail.com>
2019-04-18 16:14:40 -0400

    added secret room random roll

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,6 +14,7 @@ public class Main {
         Scanner keyboard = new Scanner(System.in);
         Random rand = new Random();
 
+        int roomSecret = rand.nextInt(5);
         int ghost = rand.nextInt(5);
         String userInput;
         boolean house = true;
@@ -43,13 +44,13 @@ public class Main {
                 break;
             case 6: roomName = "Vault";
                     roomContent = "3 Walking Seletons";
-                    direction = "";
+                    direction = "East";
                 break;
             case 7: roomName = "Parlor";
                 roomContent = "a Treasure Chest";
                 direction = "West, South";
                 break;
-            case 8: roomName = "Secret Room";
+            case 8: roomName = "Secret";
                     roomContent = "Piles of Gold";
                     direction = "West";
                 break;
@@ -58,6 +59,9 @@ public class Main {
         System.out.println("\nYou are in the " + roomName + " room.");
         System.out.println("You see " + roomContent + ".");
         System.out.println("You can go " + direction + " or leave the house.");
+        if (roomNumber == 6 && roomSecret == 4){
+            System.out.println("You found a secret room. Type (Enter)");
+        }
         System.out.print("Where will you go?\n(" + direction + " or Run) : ");
 
         while (house == true){
@@ -138,8 +142,12 @@ public class Main {
             }
             if (roomNumber == 6){
 
+
                 if (userInput.equalsIgnoreCase("east")){
                     room(7);
+                }
+                else if (userInput.equalsIgnoreCase("enter") && roomSecret == 4){
+                    room(8);
                 }
                 else if (userInput.equalsIgnoreCase("run")){
                     house = false;
@@ -190,7 +198,7 @@ public class Main {
 
 
 
-        room(1);
+        room(6);
 
 
 
